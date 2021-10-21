eb7e126108e777a61f7f23d501aac0bf25510755
markelovdn <markelovdn@gmail.com>
2021-10-21 08:00:14 +0300

    1

--- resources/views/coach/show.blade.php
+++ resources/views/coach/show.blade.php
@@ -1,12 +1,1 @@
-@extends('layouts.app')
-
-@section('content')
-    <div class="container">
-        <a href="{{route('athlete.index')}}" class="btn btn-primary">Назад</a>
-        <hr>
-        <ul>
-            <li>Тренер: {{$coach->secondname or ''}}</li>
-            <li>User name: {{$coach->athlete->secondname or ''}}</li>
-        </ul>
-    </div>
-@endsection
+show coach
